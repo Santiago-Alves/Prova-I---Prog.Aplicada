2937b6a440183832919c307e8761f2f52a5b1a4f
SanAlves <>
2021-04-17 19:53:09 +0000

    Refiz usando if

--- main.cpp
+++ main.cpp
@@ -9,85 +9,79 @@ int main() {
   cin >> entrada;
 int cont = 0;
   for (int i =0; i < 20; i++) {
-    switch(entrada[i]){
-     
-          case '0': 
-          auxiliar[cont] = 'Z';
-          cont++;
-          
-          auxiliar[cont] = 'A';
-          cont++;
-                    break;
-          case '1': 
-          auxiliar[cont] = 'Z';
-          cont++;
-          auxiliar[cont] = 'B';
-          cont++;
-                    break;
-          case '2': 
-          auxiliar[cont] = 'Z';
-          cont++;
-          auxiliar[cont] = 'C';
-          cont++;
-                    break;
-          case '3': 
-          auxiliar[cont] = 'Z';
-          cont++;
-          auxiliar[cont] = 'D';
-          cont++;
-                    break;
-          case '4': 
-          auxiliar[cont] = 'Z';
-          cont++;
-          auxiliar[cont] = 'E';
-          cont++;
-                    break;
-          case '5': 
-          auxiliar[cont] = 'Z';
-          cont++;
-          auxiliar[cont] = 'F';
-          cont++;
-                    break;
-          case '6': 
-          auxiliar[cont] = 'Z';
-          cont++;
-          auxiliar[cont] = 'G';
-          cont++;
-                    break;
-          case '7': 
-          auxiliar[cont] = 'Z';
-          cont++;
-          auxiliar[cont] = 'H';
-          cont++;
-                    break;
-          case '8': 
-          auxiliar[cont] = 'Z';
-          cont++;
-          auxiliar[cont] = 'I';
-          cont++;
-                    break;
-          case '9': 
-          auxiliar[cont] = 'Z';
-          cont++;
-          auxiliar[cont] = 'J';
-          cont++;
-                    break;  
-          
-          case 'Z':
-          auxiliar[cont] = 'Z';
-          cont++;
-          auxiliar[cont] = 'Z';
-          cont++;
-          break;  
 
-      /*    default : 
-            if (entrada[i] != 'Z') {
-              auxiliar[cont] = entrada[i] ;
+    if (entrada[i] == 'Z') {
+              auxiliar[cont] = 'Z';
               cont++;
-            } 
-            break;      */                                              
-        }
-  }
-  
-  
+              auxiliar[cont] = 'Z';
+              cont++;
+    }
+              if(entrada[i] == 'A' || entrada[i] == 'B' || entrada[i] == 'C' ||entrada[i] == 'D' ||entrada[i] == 'E' ||entrada[i] == 'F' ||entrada[i] == 'G' ||entrada[i] == 'H' ||entrada[i] == 'I' ||entrada[i] == 'J' ||entrada[i] == 'K' ||entrada[i] == 'L' ||entrada[i] == 'M' ||entrada[i] == 'N' ||entrada[i] == 'O' ||entrada[i] == 'P' ||entrada[i] == 'Q' ||entrada[i] == 'R' ||entrada[i] == 'S' ||entrada[i] == 'T' ||entrada[i] == 'U' ||entrada[i] == 'V' ||entrada[i] == 'W' ||entrada[i] == 'X' ||entrada[i] == 'Y' ) {
+                auxiliar[cont] = entrada[i] ;
+                cont++;
+              }if(entrada[i] == '0'){
+                auxiliar[cont] = 'Z';
+                cont++;
+                auxiliar[cont] = 'A';
+                cont++;
+              }
+              if(entrada[i] == '1'){
+                auxiliar[cont] = 'Z';
+                cont++;
+                auxiliar[cont] = 'B';
+                cont++;
+              }
+              if(entrada[i] == '2'){
+                auxiliar[cont] = 'Z';
+                cont++;
+                auxiliar[cont] = 'C';
+                cont++;
+              }
+              if(entrada[i] == '3'){
+                auxiliar[cont] = 'Z';
+                cont++;
+                auxiliar[cont] = 'D';
+                cont++;
+              }
+              if(entrada[i] == '4'){
+                auxiliar[cont] = 'Z';
+                cont++;
+                auxiliar[cont] = 'E';
+                cont++;
+              }
+              if(entrada[i] == '5'){
+                auxiliar[cont] = 'Z';
+                cont++;
+                auxiliar[cont] = 'F';
+                cont++;
+              }
+              if(entrada[i] == '6'){
+                auxiliar[cont] = 'Z';
+                cont++;
+                auxiliar[cont] = 'G';
+                cont++;
+              }
+              if(entrada[i] == '7'){
+                auxiliar[cont] = 'Z';
+                cont++;
+                auxiliar[cont] = 'H';
+                cont++;
+              }
+              if(entrada[i] == '8'){
+                auxiliar[cont] = 'Z';
+                cont++;
+                auxiliar[cont] = 'I';
+                cont++;
+              }
+              if(entrada[i] == '9'){
+                auxiliar[cont] = 'Z';
+                cont++;
+                auxiliar[cont] = 'J';
+                cont++;
+              }
+                         
+}
+for(int i = 0; i < cont; i++){
+ if(auxiliar )
+}
 }
